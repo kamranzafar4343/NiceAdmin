6ee46dcfdad993a7196a37c56dda0c1d128f939d
Malik Kashif <kaptaan813@gmail.com>
2024-10-01 11:13:12 +0500

    refactor

--- config/db.php
+++ config/db.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$HOSTNAME= 'localhost';
-$USERNAME='root';
-$PASSWORD='';
-$DATABASE='catmarketing';
+$HOSTNAME= 'sql12.freemysqlhosting.net';
+$USERNAME='sql12733988';
+$PASSWORD='uUqxLbg8KS';
+$DATABASE='sql12733988';
 
 $conn= mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
 

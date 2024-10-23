b75f9a9bd9d74c145f2fa3e2a000224d7f4ec07e
Kamran Zafar <kamranzafar4343@gmail.com>
2024-10-23 15:48:24 +0500

    made branch info card

--- Branches.php
+++ Branches.php
@@ -528,7 +528,7 @@ if ($result2->num_rows > 0) {
                                     <td>
                                         <div style="display: flex; gap: 10px;">
 
-                                            <a type="button" class="btn btn-success btn-success d-flex justify-content-center " style="width:25px; height: 28px;" href=".php?id=<?php echo $row['branch_id']; ?>"><i style="width: 20px;" class="fa-solid fa-file-lines"></i></a>
+                                            <a type="button" class="btn btn-success btn-success d-flex justify-content-center " style="width:25px; height: 28px;" href="branchInfo.php?id=<?php echo $row['branch_id']; ?>"><i style="width: 20px;" class="fa-solid fa-file-lines"></i></a>
                                             <a type="button" class="btn btn-success btn-info d-flex justify-content-center " style="width:25px; height: 28px;" href="branchUpdate.php?id=<?php echo $row['branch_id']; ?>"><i style="width: 20px;" class="fa-solid fa-pen-to-square"></i></a>
                                             
                                             <a type="button" class="btn btn-danger btn-floating d-flex justify-content-center" style="width:25px; height:28px" data-mdb-ripple-init

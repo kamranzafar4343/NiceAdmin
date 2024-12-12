a17a80f1946e881d835519300f7ef3e5d7705ff9
Kamran Zafar <kamranzafar4343@gmail.com>
2024-12-12 16:52:29 +0500

    for search options in dropdowns applied dselect on the company, branch, department and box input field

--- createitem.php
+++ createitem.php
@@ -42,7 +42,7 @@ if (isset($_POST["import"])) {
 
     // Get box barcode from form
     $new_barcode = mysqli_real_escape_string($conn, $_POST['box_FK_item']);
-    
+
     // batch_id = unix timestamp
     $batch_id = time();
 
@@ -126,6 +126,13 @@ if (isset($_POST["import"])) {
     <link href="assets/img/dtl.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap JS (with Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
@@ -183,6 +190,12 @@ if (isset($_POST["import"])) {
     <!-- ALERTIFY CSS -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/bootstrap.rtl.min.css" />
+
+    <script src="https://code.jquery.com/jquery/3.7.1/jquery.min.js"></script>
+
+    <!-- dselect -->
+    <link rel="stylesheet" href="assets/css/dselect.css">
+    <script src="assets/js/dselect.js"></script>
 
     <style>
         /* Custom CSS to decrease font size of the table */
@@ -397,118 +410,12 @@ if (isset($_POST["import"])) {
     $role = $_SESSION['role'];
     ?>
 
-    <!-- ======= Sidebar ======= -->
-    <aside id="sidebar" class="sidebar">
-        <ul class="sidebar-nav" id="sidebar-nav">
 
-            <!-- Dashboard Link (Visible to all users) -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="index.php">
-                    <i class="ri-home-8-line"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li><!-- End Dashboard Nav -->
-
-            <?php if ($_SESSION['role'] == 'admin') { ?>
-                <!-- Admin-only Links -->
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="Companies.php">
-                        <i class="ri-building-4-line"></i><span>Companies</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Companies Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="box.php">
-                        <i class="ri-archive-stack-fill"></i><span>Boxes</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Boxes Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link active" href="showItems.php">
-                        <i class="ri-shopping-cart-line"></i><span>Items</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Items Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="order.php">
-                        <i class="ri-list-ordered"></i><span>Work Orders</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Work Orders Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="racks.php">
-                        <i class="bi bi-box"></i><span>Racks</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Racks Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="store.php">
-                        <i class="bi bi-shop"></i><span>Store</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Store Nav -->
-
-            <?php } else { ?>
-                <!-- User-only Links -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="box.php">
-                        <i class="ri-archive-stack-fill"></i><span>Boxes</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Boxes Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link active" href="showItems.php">
-                        <i class="ri-shopping-cart-line"></i><span>Items</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Items Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="order.php">
-                        <i class="ri-list-ordered"></i><span>Work Orders</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Work Orders Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="racks.php">
-                        <i class="bi bi-box"></i><span>Racks</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Racks Nav -->
-
-                <li class="nav-item">
-                    <a class="nav-link collapsed" href="store.php">
-                        <i class="bi bi-shop"></i><span>Store</span><i class="bi bi-chevron ms-auto"></i>
-                    </a>
-                </li><!-- End Store Nav -->
-            <?php } ?>
-
-
-            <li class="nav-heading">Pages</li>
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="users-profile.php">
-                    <i class="bi bi-person"></i><span>Profile</span>
-                </a>
-            </li><!-- End Profile Nav -->
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-login.php">
-                    <i class="bi bi-box-arrow-right"></i><span>Login</span>
-                </a>
-            </li><!-- End Login Nav -->
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="logout.php">
-                    <i class="bi bi-box-arrow-left"></i><span>Logout</span>
-                </a>
-            </li><!-- End Logout Nav -->
-
-        </ul>
-    </aside>
-    <!--------------- End sidebar ------------------>
-
-
-    <!-- ---------------------------------------------------End Sidebar--------------------------------------------------->
-
+    <!-- sidebar start -->
+    <?php
+    include "sidebarcode.php";
+    ?>
+    <!-- sidebar end -->
 
     <!--form--------------------------------------form--------------------------------------->
     <!-- Start Header form -->
@@ -579,62 +486,53 @@ if (isset($_POST["import"])) {
                     </div>
                 </form>
                 <div class="container mt-5">
-        <table class="table table-striped table-bordered table-hover">
-            <thead class="bg-primary text-white">
-                <tr>
-                    <th>#</th>
-                    <th>Box Barcode</th>
-                    <th>File Number</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $i = 1;
-                $rows = mysqli_query($conn, "SELECT * FROM item WHERE batch_id = (SELECT MAX(batch_id) FROM item)");
-                foreach ($rows as $row) :
-                ?>
-                <tr>
-                    <td><?php echo $i++; ?></td>
-                    <td><?php echo htmlspecialchars($row["box_barcode"]); ?></td>
-                    <td><?php echo htmlspecialchars($row["file_no"]); ?></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead class="bg-primary text-white">
+                            <tr>
+                                <th>#</th>
+                                <th>Box Barcode</th>
+                                <th>File Number</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            $rows = mysqli_query($conn, "SELECT * FROM item WHERE batch_id = (SELECT MAX(batch_id) FROM item)");
+                            foreach ($rows as $row) :
+                            ?>
+                                <tr>
+                                    <td><?php echo $i++; ?></td>
+                                    <td><?php echo htmlspecialchars($row["box_barcode"]); ?></td>
+                                    <td><?php echo htmlspecialchars($row["file_no"]); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-    <!-- Include Bootstrap JS (with Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="assets/vendor/echarts/echarts.min.js"></script>
-    <script src="assets/vendor/quill/quill.js"></script>
-    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js">
-    </script>
-    <!-- Bootstrap JS (Optional) -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7/z1gk35k1RA6QQg+SjaK6MjpS3TdeL1h1jDdED5+ZIIbsSdyX/twQvKZq5uY15B" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9BfDxO4v5a9J9TZz1ck8vTAvO8ue+zjqBd5l3eUe8n5EM14ZlXyI4nN" crossorigin="anonymous"></script>
     <!-- Template Main JS File -->
 
 
     <script>
         $(document).ready(function() {
 
-         
+
+            const config = {
+                search: true, // Enable search feature
+                creatable: false, // Disable creatable selection
+                clearable: false, // Disable clearable selection
+                maxHeight: '360px', // Max height for showings scrollbar
+                size: 'md', // Size of the select, can be 'sm' or 'lg'
+            };
+
+            // Initialize dselect for the initial dropdowns
+            dselect(document.querySelector('#company'), config);
+            dselect(document.querySelector('#branch'), config);
+            dselect(document.querySelector('#dept'), config);
+            dselect(document.querySelector('#box'), config);
 
             // When company is changed, fetch the branches
             $('#company').change(function() {
@@ -657,6 +555,9 @@ if (isset($_POST["import"])) {
                             $.each(branches, function(index, branch) {
                                 $('#branch').append('<option value="' + branch.branch_id + '">' + branch.branch_name + '</option>');
                             });
+                            // Refresh or reinitialize dselect
+                            dselect(document.querySelector('#branch'), config);
+
                         } catch (e) {
                             console.error("Invalid JSON response", response);
                         }
@@ -689,7 +590,10 @@ if (isset($_POST["import"])) {
                                 $('#box').append('<option value="' + box.barcode + '">' + box.barcode + '</option>');
 
                             });
-                      
+                            // Refresh or reinitialize dselect
+                            dselect(document.querySelector('#box'), config);
+
+
                         } catch (e) {
                             console.error("Invalid JSON response", response);
                         }
@@ -720,7 +624,9 @@ if (isset($_POST["import"])) {
                             $.each(departments, function(index, department) {
                                 $('#dept').append('<option value="' + department.dept_id + '">' + department.dept_name + '</option>');
                             });
-                        
+                            // Refresh or reinitialize dselect
+                            dselect(document.querySelector('#dept'), config);
+
                         } catch (e) {
                             console.error("Invalid JSON response", response);
                         }
@@ -748,6 +654,40 @@ if (isset($_POST["import"])) {
     endif;
     ?>
 
+    <!-- Include Bootstrap JS (with Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="assets/vendor/echarts/echarts.min.js"></script>
+    <script src="assets/vendor/quill/quill.js"></script>
+    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <!--datatable export buttons-->
+    <script src="https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.2/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.print.min.js"></script>
+
+    <!-- Bootstrap JS (Optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7/z1gk35k1RA6QQg+SjaK6MjpS3TdeL1h1jDdED5+ZIIbsSdyX/twQvKZq5uY15B" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9BfDxO4v5a9J9TZz1ck8vTAvO8ue+zjqBd5l3eUe8n5EM14ZlXyI4nN" crossorigin="anonymous"></script>
     <script src="assets/js/main.js"></script>
 </body>
 

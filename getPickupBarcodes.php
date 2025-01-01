742062a5889856ec728036fd535d1128ba789dd7
Kamran Zafar <kamranzafar4343@gmail.com>
2025-01-01 15:10:41 +0500

    only fetch boxes whch are out

---
+++ getPickupBarcodes.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'config/db.php';
+if (isset($_POST['branch_id'])) {
+    $branch_id = $_POST['branch_id'];
+
+    // Simple SQL query to get barcodes for the selected branches
+    $result = $conn->query("SELECT * FROM box WHERE branch_id_fk = '$branch_id' AND location IS NOT NULL AND status = 'Out';");
+
+    $boxes = array();
+    while ($row = $result->fetch_assoc()) {
+        $boxes[] = $row;
+    }
+
+    // Return branches as JSON
+    echo json_encode($boxes);
+
+    $conn->close();
+}

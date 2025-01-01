6cea394ba85671e29a0e9d5e2f3b2cd96930953a
Kamran Zafar <kamranzafar4343@gmail.com>
2025-01-01 11:39:01 +0500

    only fetch those barcodes which has 'In' status

--- getBarcodes.php
+++ getBarcodes.php
@@ -5,7 +5,7 @@ if (isset($_POST['branch_id'])) {
     $branch_id = $_POST['branch_id'];
 
     // Simple SQL query to get barcodes for the selected branches
-    $result = $conn->query("SELECT * FROM box WHERE branch_id_fk = '$branch_id' AND location IS NOT NULL;");
+    $result = $conn->query("SELECT * FROM box WHERE branch_id_fk = '$branch_id' AND location IS NOT NULL AND status = 'In';");
 
     $boxes = array();
     while ($row = $result->fetch_assoc()) {
